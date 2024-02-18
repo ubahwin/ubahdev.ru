e6c1c3f3a685ed2ec1c15ba7b1c712748fbf28c0
Ivan Vdovin <ivan03_vdovin@mail.ru>
2024-02-18 23:15:12 +0300

    initial commit

---
+++ src/pages/Articles.tsx
@@ -0,0 +1,13 @@
+import '@styles/Articles.scss'
+
+const Articles = () => {
+  return (
+    <>
+      <div className="articles-container">
+        In future...
+      </div>
+    </>
+  )
+}
+
+export default Articles
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  build: {
+    target: 'esnext',
+    minify: 'terser'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@pages': path.resolve(__dirname, 'src/pages')
+    }
+  },
+  plugins: [react()],
+})
